c43f46f27e8cecc807d563c914bcdb59267458ea
SorielV <xxsorielxx@gmail.com>
2019-09-28 19:53:12 +0000

    Improve data structure in campusclubs.json file. Fix #160

--- src/components/groups.js
+++ src/components/groups.js
@@ -1,34 +1,33 @@
 import React from 'react'
 
-const Groups = ({ name, data, description }) =>(
+const Groups = ({ name, data, description }) => {
+  console.log(data)
+  return (
     <div className="tile is-ancestor is-vertical">
-                    <div className="tile is-parent">
-                        <div className="tile is-child box">
-                            <div className="tile is-ancestor is-vertical">
-                                <div className="tile is-parent is-vertical">
-                                    <p className="title">{name}</p>
-                                    {description}
-                                </div>
-        <div className="columns is-multiline">
-    {data.edges.map(({node})=>(
-        <div className="column is-4" key={node.id}>
-            <div className="tile is-child box">
-                <p className="subtitle">{node.name}</p>
-                {node.website?<a href={node.website}>Website </a> :null}
-                {node.telegram?<a href={node.telegram}>Telegram </a> :null}
-                {node.twitter?<a href={node.twitter}>Twitter </a> :null}
-                {node.GitHub?<a href={node.GitHub}>GitHub </a> :null}
-                {node.instagram?<a href={node.instagram}>Instagram </a> :null}
-                {node.facebook?<a href={node.facebook}>Facebook </a> :null}
-                {node.meetup?<a href={node.meetup}>Meetup </a> :null}
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <div className="tile is-ancestor is-vertical">
+            <div className="tile is-parent is-vertical">
+              <p className="title">{name}</p>
+              {description}
             </div>
+            <div className="columns is-multiline">
+              {data.edges.map(({ node }) => (
+                <div className="column is-4" key={node.id}>
+                  <div className="tile is-child box">
+                    <p className="subtitle">{node.name}</p>
+                    {node.social.map(social => (
+                      <a href={social.link}>{social.name} </a>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    ))}
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    </div>
-)
+  )
+}
 
 export default Groups
